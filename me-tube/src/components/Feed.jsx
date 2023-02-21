f8f9547e93e87b36ad6fd02e8db71c0999088000
thehumblecodergp <gouravpratap.202cd006@nitk.edu.in>
2023-02-21 10:47:25 +0530

    channel Card

--- me-tube/src/components/Feed.jsx
+++ me-tube/src/components/Feed.jsx
@@ -17,7 +17,7 @@ const Feed = () => {
   return (
     <Stack sx = {{ flexDirection: { sx: "column", md: "row"}}}>
       <Box
-        sx={{ height: {sx: 'auto', md: '92vh' }, 
+        sx={{ height: {sx: 'auto', md: '100%' }, 
           borderRight: '1px solid #3d3d3d', 
           px: { sx: 0, md: 2}}}
       >
@@ -27,7 +27,7 @@ const Feed = () => {
         </Typography>
       </Box>
 
-      <Box p={2} sx = {{overFlowY: 'auto', height: '90vh', flex: 2}}>
+      <Box p={2} sx = {{overFlowY: 'auto', height: '100%', flex: 2}}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
           {selectedCategory} <span style={{color: 'blue'}}>Videos</span>
         </Typography>
